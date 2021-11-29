04d0be119f3e39d959d32587abda1479ee6c88ed
nlferman <nferman@xperience-det.com>
2021-11-29 15:35:20 -0500

    Removed BroweserRouter fromm navigation page and replaced Links with a tags so anchor links work properly.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,41 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
 import './navigation.css';
 import GPT3 from '../../images/GPT-3.png';
 
 const Navigation = () => {
-
     return (
-        <BrowserRouter>
-            <div className='navbar flex'>
-                <img className='logo' src={GPT3} alt='GPT-3'/>
-                <div className='nav-container flex'>
-                    <nav>
-                        <ul id='primary-navigation' className='primary-navigation flex'>
-                            <li className='active'>
-                                <Link className='navigation-text' to='#'>Home</Link>
-                            </li>
-                            <li>
-                                <Link className='navigation-text' to='#GTP'>What is GPT?</Link>
-                            </li>
-                            <li>
-                                <Link className='navigation-text' to='#OpenAI'>OpenAI</Link>
-                            </li>
-                            <li>
-                                <Link className='navigation-text' to='#CaseStudies'>Case Studies</Link>
-                            </li>
-                            <li>
-                                <Link className='navigation-text' to='#Library'>Library</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div className='sign-up'>
-                        <label className='sign-in-text'>Sign in</label>
-                        <button className='sign-up-btn'>Sign up</button>
-                    </div>
+        <div className='navbar flex'>
+            <img className='logo' src={GPT3} alt='GPT-3'/>
+            <div className='nav-container flex'>
+                <nav>
+                    <ul id='primary-navigation' className='primary-navigation flex'>
+                        <li className='active'>
+                            <a className='navigation-text' href='#'>Home</a>
+                        </li>
+                        <li>
+                            <a className='navigation-text' href='#GTP'>What is GPT?</a>
+                        </li>
+                        <li>
+                            <a className='navigation-text' href='#OpenAI'>OpenAI</a>
+                        </li>
+                        <li>
+                            <a className='navigation-text' href='#CaseStudies'>Case Studies</a>
+                        </li>
+                        <li>
+                            <a className='navigation-text' href='#Library'>Library</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className='sign-up'>
+                    <label className='sign-in-text'>Sign in</label>
+                    <button className='sign-up-btn'>Sign up</button>
                 </div>
             </div>
-        </BrowserRouter>
+        </div>
     )
 }
 
